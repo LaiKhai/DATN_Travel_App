41703cecaec394a7aeb46be1741eb5acf79e4a56
chaungo.100801@gmail.com <chaungo.100801@gmail.com>
2022-06-01 00:31:55 +0700

    commit api get tour by id

--- Travel/Controllers/ToursController.cs
+++ Travel/Controllers/ToursController.cs
@@ -56,7 +56,6 @@ namespace Travel.Controllers
                 int idtour = tour.Id;
 
                 //Insert dia diem   
-                
                 foreach (var dd in tourCreate.rDiaDiem)
                 {
                     var diadiem = new DiaDiem();
@@ -68,7 +67,6 @@ namespace Travel.Controllers
                 }
 
                 //insert thoi gian
-                
                 foreach (var tt in tourCreate.rThoiGians)
                 {
                     var thoigian = new ThoiGian();
@@ -107,7 +105,6 @@ namespace Travel.Controllers
                 _context.SaveChanges();
 
                 //insert ghi chu
-                
                 foreach (var gc in tourCreate.rGhiChus)
                 {
                     var ghichu = new GhiChu();
@@ -120,7 +117,6 @@ namespace Travel.Controllers
                 }
 
                 //insert anh tour
-                
                 foreach (var at in tourCreate.rAnhTour)
                 {
                     var anhtour = new AnhTour();
@@ -141,9 +137,6 @@ namespace Travel.Controllers
                     _context.Add(anhtour);
                     _context.SaveChanges();
                 }
-
-
-
                 // Commit transaction if all commands succeed, transaction will auto-rollback
                 // when disposed if either commands fails
                 transaction.Commit();
@@ -160,39 +153,161 @@ namespace Travel.Controllers
         [ActionName("getTour")]
         public async Task<IActionResult> getTour()
         {
-            List<getTours> result = new List<getTours>();
-            List<Tour> tours = _context.Tours.Include(t => t.TheLoai).Include(t=>t.CongTy).Where(t => t.TrangThai == 1).ToList();
-            foreach(var tour in tours)
+            try
             {
-                getTours gt = new getTours();
-                gt.tenTour = tour.TenTour;
-                gt.id = tour.Id;
-                gt.theLoaiId = tour.TheLoaiId;
-                gt.Tenloai = tour.TheLoai.TenLoai;
-                gt.TencongTy = tour.CongTy.Tencongty;
-                gt.congTyId = tour.CongTyId;
-                gt.moTa = tour.MoTa;
-                gt.canChuanBi = tour.CanChuanBi;
-                gt.diemNoiBat = tour.DiemNoiBat;
-                gt.diemDi = tour.DiemDi;
-                gt.ngayTao = tour.NgayTao;
-                var anhtour = _context.AnhTours.FirstOrDefault(p => p.TourId == tour.Id);
-                if (anhtour == null)
+                List<getTours> result = new List<getTours>();
+                List<Tour> tours = _context.Tours.Include(t => t.TheLoai).Include(t => t.CongTy).Where(t => t.TrangThai == 1).ToList();
+                foreach (var tour in tours)
                 {
-                    gt.anhTours = "default.jpg";
-                }
-                else
-                {
-                    gt.anhTours = anhtour.Anh;
-                }
-                var gia = _context.ThoiGians.OrderByDescending(p=>p.GiaDefaut).FirstOrDefault(p => p.TourId == tour.Id);
-                gt.Gia = gia.GiaDefaut;
+                    getTours gt = new getTours();
+                    gt.tenTour = tour.TenTour;
+                    gt.id = tour.Id;
+                    gt.theLoaiId = tour.TheLoaiId;
+                    gt.Tenloai = tour.TheLoai.TenLoai;
+                    gt.TencongTy = tour.CongTy.Tencongty;
+                    gt.congTyId = tour.CongTyId;
+                    gt.moTa = tour.MoTa;
+                    gt.canChuanBi = tour.CanChuanBi;
+                    gt.diemNoiBat = tour.DiemNoiBat;
+                    gt.diemDi = tour.DiemDi;
+                    gt.ngayTao = tour.NgayTao;
+                    var anhtour = _context.AnhTours.FirstOrDefault(p => p.TourId == tour.Id);
+                    if (anhtour == null)
+                    {
+                        gt.anhTours = "default.jpg";
+                    }
+                    else
+                    {
+                        gt.anhTours = anhtour.Anh;
+                    }
+                    var gia = _context.ThoiGians.OrderByDescending(p => p.GiaDefaut).FirstOrDefault(p => p.TourId == tour.Id);
+                    gt.Gia = gia.GiaDefaut;
 
-                result.Add(gt);
-            }    
-            return Json(result);
+                    result.Add(gt);
+                }
+                return Json(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "error occurred" });
+            }
 
         }
 
+        [HttpGet]
+        [Route("{id:int}")]
+        [ActionName("getaTour")]
+        public async Task<IActionResult> getaTour([FromRoute] int id)
+        {
+            try
+            {
+                // get tour
+                var tour = _context.Tours.Include(t => t.TheLoai).Include(t => t.CongTy).FirstOrDefault(t => t.Id == id);
+                if (tour == null)
+                {
+                    throw new Exception();
+                }
+                //get anh tour
+                List<string> anhtours = new List<string>();
+                var anhtour = _context.AnhTours.Where(a => a.TourId == id && a.TrangThai == 1).ToList();
+                foreach (AnhTour a in anhtour)
+                {
+                    anhtours.Add(a.Anh);
+                }
+                var ctdv = _context.ChiTietDichVus.FirstOrDefault(c => c.TourId == id && c.TrangThai == 1);
+                if (ctdv == null)
+                {
+                    throw new Exception();
+                }
+                //get dia diem
+                List<object> diadiems = new List<object>();
+                List<string> anhdd = new List<string>();
+                var diadiem = _context.DiaDiems.Include(a => a.AnhDds).Where(a => a.TourId == id && a.TrangThai == 1).ToList();
+                foreach (DiaDiem d in diadiem)
+                {
+                    var anhdds = _context.AnhDds.Where(a => a.DiaDiemId == d.Id && a.TrangThai == 1).ToList();
+                    foreach (AnhDd add in anhdds)
+                    {
+                        anhdd.Add(add.Anh);
+                    }
+                    var result = new { diadiemid = d.Id, tendiadiem = d.Ten, hinhanh = anhdd };
+                    diadiems.Add(result);
+                }
+                //get gia tre em
+                List<object> giatreems = new List<object>();
+                var giatreem = _context.GiaTreEms.Where(gte => gte.TourId == id && gte.TrangThai == 1).ToList();
+                foreach (GiaTreEm gte in giatreem)
+                {
+                    var result = new { giatreemid = gte.Id, dotuoi = gte.DoTuoi, giave = gte.GiaVe };
+                    giatreems.Add(result);
+                }
+                //get thoi gian
+                List<object> thoigians = new List<object>();
+                var thoigian = _context.ThoiGians.Where(tg => tg.TourId == id && tg.TrangThai == 1).ToList();
+                foreach (ThoiGian tg in thoigian)
+                {
+                    var result = new { thoigianid = tg.Id, ngaydi = tg.NgayDi, ngayve = tg.NgayVe, giadefault = tg.GiaDefaut };
+                    thoigians.Add(result);
+                }
+                //get lich trinh
+                List<object> lichtrinhs = new List<object>();
+                var lichtrinh = _context.LichTrinhs.Where(lt => lt.TourId == id && lt.TrangThai == 1).ToList();
+                foreach (LichTrinh lt in lichtrinh)
+                {
+                    var result = new { lichtrinhid = lt.Id, sang = lt.Sang, trua = lt.Trua, chieu = lt.Chieu };
+                    lichtrinhs.Add(result);
+                }
+                //get ghi chu
+                List<object> ghichus = new List<object>();
+                var ghichu = _context.GhiChus.Where(gc => gc.TourId == id && gc.TrangThai == 1).ToList();
+                foreach (GhiChu gc in ghichu)
+                {
+                    var result = new { ghichuid = gc.Id, tieude = gc.TieuDe, noidung = gc.NoiDung };
+                    ghichus.Add(result);
+                }
+                //get khuyen mai
+                List<object> khuyenmais = new List<object>();
+                var khuyenmai = _context.KhuyenMais.Where(km => km.TourId == id && km.NgayHetHan <= DateTime.Now && km.TrangThai == 1).ToList();
+                foreach (KhuyenMai km in khuyenmai)
+                {
+                    var result = new { khuyenmaiid = km.Id, tenkhuyenmai = km.TenKm, giatri = km.GiaTri, ngayapdung = km.NgayApDung, ngayhethan = km.NgayHetHan };
+                    khuyenmais.Add(result);
+                }
+                return Json(new
+                {
+                    tentour = tour.TenTour,
+                    theloaiid = tour.TheLoaiId,
+                    Tenloai = tour.TheLoai.TenLoai,
+                    congtyid = tour.CongTyId,
+                    Tencongty = tour.CongTy.Tencongty,
+                    congty_email = tour.CongTy.Email,
+                    congty_mst = tour.CongTy.Mst,
+                    congty_sdt = tour.CongTy.Sdt,
+                    congty_vanphong = tour.CongTy.VanPhong,
+                    congty_khuvuc = tour.CongTy.KhuVuc,
+                    congty_thenganhang = tour.CongTy.TheNganHang,
+                    mota = tour.MoTa,
+                    canchuanbi = tour.CanChuanBi,
+                    diemnoibat = tour.DiemNoiBat,
+                    diemdi = tour.DiemDi,
+                    ctdvid = ctdv.Id,
+                    dichvu_cotrongve = ctdv.CoTrongVe,
+                    dichvu_khongcotrongve = ctdv.KhongTrongVe,
+                    dichvu_chedotrem = ctdv.CheDoTreEm,
+                    anhtour = anhtours,
+                    diadiem = diadiems,
+                    giatreem = giatreems,
+                    thoigian = thoigians,
+                    lichtrinh = lichtrinhs,
+                    ghichu = ghichus,
+                    khuyenmai = khuyenmais
+
+                });
+            }
+            catch (Exception)
+            {
+                return NotFound("Tourid does not existed");
+            }
+        }
     }
 }
